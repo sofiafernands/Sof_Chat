659d3e1805d0be02b8e03023e0e039d8b186332c
Sofia Fernandes <123645624+sofiafernands@users.noreply.github.com>
2023-10-16 16:59:05 +0000

    continuacion chat

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # node.js
 
 *instalamos depencias express y morgan 
-*en el index JS (el servidor(carpeta server))
+*en el index JS (el servidor(carpeta server)) 
+* usamos morgan para que nos de la informacion en la terminal
+
--- server/index.js
+++ server/index.js
@@ -1,16 +1,35 @@
 import express from "express";
-import logger from "morgan";
+import logger from "morgan"; 
+
+import { createServer } from "node:http"; //importa el modulo http de node
+import { Server } from "socket.io"; //importa el modulo socket.io
 
 const port = process.env.PORT ?? 3000
 
 const app = express();
+const server = createServer(app); // create an http server with express
+const io = new Server(server, {
+    connectionStateRecovery : {}
+}); //creates a socket server with the http server (const server)
+
+//connection new user
+io.on('connection', (socket) => {
+    console.log('New connection');
+//disconnet user
+    socket.on('disconnect', () => {
+        console.log('User disconnected')
+    })
+    socket.on('chat message', (msg) => {
+        io.emit('chat message',  msg)})
+});
+
 app.use(logger('dev'));
 
 app.get('/', (req, res) => {
-    //cwd(currente working directory) es la carpeta en la que se inicializado el proceso(node) en este caso es la carpeta client
+    //cwd(currente working directory) is the folder in which the process(node) has been initialized, in this case it is the client folder.
     res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Server running on port ${port}')
 })
